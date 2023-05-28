f3a59169fe3ca31acc874819e319b46ee09fdbda
Rik Gissel <rikgissel2001@gmail.com>
2023-05-28 01:34:57 +0100

    Fagvaelger changes

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # trivSelv
  Main Repository 
+
+```
+trivselv-main
+├─ client
+│  ├─ package-lock.json
+│  ├─ package.json
+│  ├─ public
+│  │  ├─ favicon.ico
+│  │  ├─ index.html
+│  │  ├─ logo192.png
+│  │  ├─ logo512.png
+│  │  ├─ manifest.json
+│  │  └─ robots.txt
+│  ├─ README.md
+│  └─ src
+│     ├─ App.css
+│     ├─ App.js
+│     ├─ App.test.js
+│     ├─ components
+│     │  ├─ PopupJS.jsx
+│     │  └─ PopupMap.jsx
+│     ├─ css
+│     │  ├─ main.css
+│     │  └─ main.css.map
+│     ├─ fag.json
+│     ├─ index.js
+│     ├─ logo.svg
+│     ├─ reportWebVitals.js
+│     ├─ SCSS
+│     │  ├─ helpers
+│     │  ├─ layout
+│     │  ├─ main.scss
+│     │  ├─ modules
+│     │  └─ pages
+│     └─ setupTests.js
+├─ package-lock.json
+├─ package.json
+├─ README.md
+└─ server.js
+
+```
--- client/src/components/PopupMap.jsx
+++ client/src/components/PopupMap.jsx
@@ -1,53 +1,81 @@
 import React, { useState } from 'react';
-import '../css/main.css'; // Import the CSS file
+import '../css/main.css';
 
-const ContentSwitcher = () => {
-  const [activeSection, setActiveSection] = useState('Grundskole');
+const data = {
+  section1: [
+    "Tysk", "Spansk", "Engelsk", "Samfundsfag", "Matematik", "Fransk", "Drama", "Dansk", "Biologi",
+    "Madkundskab", "Historie", "Dansk som andetsprog", "Natur/Teknologi", "Fysik/kemi", "Idræt", "Musik",
+    "Billedkunst", "Kristendomkundskab", "Geografi"
+  ],
+  section2: [
+    "Billedkunst", "Dansk", "Engelsk", "Historie", "Idéhistorie", "Latin", "Mediefag", "Musik", "Oldtidskundskab", "Religion", "Retorik", "Spansk", "Fransk", "Tysk", "Astronomi", "Biologi", "Bioteknologi", "Fysik",
+    "Geografi", "Idræt", "Informatik", "IT", "Kemi", "Matematik", "Naturgeografi", "Programmering",
+    "Afsætning", "Erhvervsjura", "Erhvervsøkonomi", "Finansiering", "Innovation", "International økonomi",
+    "Psykologi", "Samfundsfag", "Virksomhedsøkonomi"
+  ],
+  section3: [
+    "Kost & Motion", "Social Trivsel", "Psykisk Trivsel", "Fysisk Trivsel", "Angst Støtte", "Digital Dannelse",
+    "Støtte Venskab", "Misbrug & Afhængighed", "ADHD/ADD-støtte", "Højt Begavede", "Sorg Støtte",
+    "Seksualvejledning", "Uddannelsesvejledning", "Stress Støtte", "Skolevægring", "Uddannelsesparathed & Info"
+  ]
+};
 
-  const handleSectionChange = (section) => {
-    setActiveSection(section);
+function ContentSwitcher() {
+  const [activeSection, setActiveSection] = useState('section1');
+
+  const handleSectionChange = (sectionId) => {
+    setActiveSection(sectionId);
   };
 
   return (
-    <div className='lessonsWrapper'>
-      <div className='lessons-txt-container'>
-        <h1>Fagområder</h1>
-        <h4>Vi søger Boostere indenfor følgende fagområder</h4>
-      </div>
-      <div className='btn-container'>
-        <button
-          onClick={() => handleSectionChange('Grundskole')}
-          className={`button ${activeSection === 'Grundskole' ? 'active' : ''} lesson-btn`}
-        >
-          Grundskole
-        </button>
-        <button
-          onClick={() => handleSectionChange('Gymnasie')}
-          className={activeSection === 'Gymnasie' ? 'active' : ''}
-        >
-          Gymnasie
-        </button>
-        <button
-          onClick={() => handleSectionChange('Trivsel')}
-          className={activeSection === 'Trivsel' ? 'active' : ''}
-        >
-          Trivsel
-        </button>
-      </div>
+    <div>
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+      </head>
+      <div className="overlay"></div>
+      <main>
+        <div className="headings-text">
+          <h1>Fagområder</h1>
+          <h4>Vi søger Boostere indenfor følgende fagområder</h4>
+        </div>
+        <section className="btns">
+          <button className="button-style" id="btn1" onClick={() => handleSectionChange('section1')}>
+            Grundskole
+          </button>
+          <button className="button-style" id="btn2" onClick={() => handleSectionChange('section2')}>
+            Gymnasie
+          </button>
+          <button className="button-style" id="button3" onClick={() => handleSectionChange('section3')}>
+            Trivsel
+          </button>
+        </section>
 
-      <div>
-        {activeSection === 'Grundskole' && (
-          <h2>Grundskole Content</h2>
-        )}
-        {activeSection === 'Gymnasie' && (
-          <h2>Gymnasie Content</h2>
-        )}
-        {activeSection === 'Trivsel' && (
-          <h2>Trivsel Content</h2>
-        )}
-      </div>
+        <div id="section1" className={`animate__animated ${activeSection === 'section1' ? 'animate__fadeIn' : 'hidden'}`}>
+          {data.section1.map((item, index) => (
+            <div key={index} className="box-1">
+              {item}
+            </div>
+          ))}
+        </div>
+
+        <div id="section2" className={`animate__animated ${activeSection === 'section2' ? 'animate__fadeIn' : 'hidden'}`}>
+          {data.section2.map((item, index) => (
+            <div key={index} className="box-2">
+              {item}
+            </div>
+          ))}
+        </div>
+
+        <div id="section3" className={`animate__animated ${activeSection === 'section3' ? 'animate__fadeIn' : 'hidden'}`}>
+          {data.section3.map((item, index) => (
+            <div key={index} className="box-3">
+              {item}
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
-};
+}
 
 export default ContentSwitcher;
