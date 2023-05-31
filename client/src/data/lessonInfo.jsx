44effcffde53a5b98a515ad4f093ce02d4d6cd56
Rik Gissel <rikgissel2001@gmail.com>
2023-05-31 23:33:21 +0100

    Fagvælger rettelser

--- client/src/data/lessonInfo.jsx
+++ client/src/data/lessonInfo.jsx
@@ -1,65 +1,93 @@
-const lessonsInfo = {
-    Tysk: {
-      title: 'Tysk',
-      languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
-      additionalRequirements: '(skal være fortsættersprog)',
-    },
-    Spansk: {
-      title: 'Spansk',
-      languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
-    },
-    Engelsk: {
-      title: 'Engelsk',
-      languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
-    },
-    Samfundsfag: {
-      title: 'Samfundsfag',
-    },
-    Matematik: {
-      title: 'Matematik',
-    },
-    Fransk: {
-        title: 'Fransk',
-        languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
-      },
-      Dansk: {
-        title: 'Dansk',
-      },
-      Biologi: {
-        title: 'Biologi',
-      },
-      Historie: {
-        title: 'Historie',
-      },
-    'Dansk som andetsprog': {
-      title: 'Dansk som andetsprog',
-      requirements:
-        'Læreruddannet med DSA kendskab\nBa i Dansk\nBa i nordisk sprog og litt.\nBa i Lingvistik\nBa i Retorik\nEller anden relevant baggrund',
-    },
-    "Natur/Teknologi": {
-        title: 'Natur/Teknologi',
-      },
-      "Fysik/Kemi": {
-        title: 'Natur/Teknologi',
-      },
-      Idræt: {
-        title: 'Idræt',
-      },
-    Musik: {
-      title: 'Musik',
-      requirements:
-        'Min. 18 år & 2.G gennemført musisk linje\nGrundlæggende musisk forståelse\nEller anden relevant baggrund',
-    },
-    Billedkunst: {
-      title: 'Billedkunst',
-      requirements: 'Min. 18 år & 2.G gennemført\nEller anden relevant baggrund',
-    },
-    Kristendomskundskab: {
-      title: 'Kristendomskundskab',
-    },
-    Geografi: {
-      title: 'Geografi',
-    },
-  };
+import React from 'react';
 
-  export default lessonsInfo;
+const lessonsInfo = {
+  Tysk: {
+    title: 'Tysk',
+    languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
+    additionalRequirements: '(skal være fortsættersprog)',
+  },
+  Spansk: {
+    title: 'Spansk',
+    languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
+  },
+  Engelsk: {
+    title: 'Engelsk',
+    languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
+  },
+  Samfundsfag: {
+    title: 'Samfundsfag',
+  },
+  Matematik: {
+    title: 'Matematik',
+  },
+  Fransk: {
+    title: 'Fransk',
+    languageSkills: 'Gode sprogkundskaber skriftligt og mundtligt',
+  },
+  Dansk: {
+    title: 'Dansk',
+  },
+  Biologi: {
+    title: 'Biologi',
+  },
+  Historie: {
+    title: 'Historie',
+  },
+  'Dansk som andetsprog': {
+    title: 'Dansk som andetsprog',
+    requirements: (
+      <>
+        Læreruddannet med DSA kendskab
+        <br />
+        Ba i Dansk
+        <br />
+        Ba i nordisk sprog og litt.
+        <br />
+        Ba i Lingvistik
+        <br />
+        Ba i Retorik
+        <br />
+        Eller anden relevant baggrund
+      </>
+    ),
+  },
+  'Natur/Teknologi': {
+    title: 'Natur/Teknologi',
+  },
+  'Fysik/Kemi': {
+    title: 'Natur/Teknologi',
+  },
+  Idræt: {
+    title: 'Idræt',
+  },
+  Musik: {
+    title: 'Musik',
+    requirements: (
+      <>
+        Min. 18 år & 2.G gennemført musisk linje
+        <br />
+        Grundlæggende musisk forståelse
+        <br />
+        Eller anden relevant baggrund
+      </>
+    ),
+  },
+  Billedkunst: {
+    title: 'Billedkunst',
+    requirements: (
+      <>
+        Min. 18 år & 2.G gennemført
+        <br />
+        Eller anden relevant baggrund
+      </>
+    ),
+  },
+  Kristendomskundskab: {
+    title: 'Kristendomskundskab',
+  },
+  Geografi: {
+    title: 'Geografi',
+  },
+};
+
+export default lessonsInfo;
